022da959ec81e065222fc69a6e23d77975ff87b0
Prince kathiriya <princekathiriya62@gmail.com>
2023-08-16 18:38:15 +0530

    ds

--- operator.c
+++ operator.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a=1,b=2,c;
-    c=a&&b;
-    printf("a:%d,b:%d,c:%d\n",a,b,c);
+    int a = 1, b = 2, c;
+    c = a && b;
+    printf("a:%d,b:%d,c:%d\n", a, b, c);
     // c=a||b;
     // printf("a:%d,b:%d,c:%d\n",a,b,c);
     // c=++a&&b;
