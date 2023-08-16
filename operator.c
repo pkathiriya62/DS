99b77008aaf190d7e074cc4e007669aaed7b6169
Prince kathiriya <princekathiriya62@gmail.com>
2023-08-16 19:00:59 +0530

    ds

--- operator.c
+++ operator.c
@@ -2,22 +2,22 @@
 int main()
 {
     int a = 1, b = 2, c=1,d;
-    //c = a && b;
-    //printf("a:%d,b:%d,c:%d\n", a, b, c);
-    // c=a||b;
-    // printf("a:%d,b:%d,c:%d\n",a,b,c);
-    // c=++a&&b;
-    // printf("a:%d,b:%d,c:%d\n",a,b,c);
-    // c=++a||b;
-    // printf("a:%d,b:%d,c:%d\n",a,b,c);
-    // c=a&&++b;
-    // printf("a:%d,b:%d,c:%d\n",a,b,c);
-    // c=a||++b;
-    // printf("a:%d,b:%d,c:%d\n",a,b,c);
-    // c=++a&&++b;
-    // printf("a:%d,b:%d,c:%d\n",a,b,c);
-    // c=++a||++b;
-    // printf("a:%d,b:%d,c:%d\n",a,b,c);
-    d=a&&b||c;
-    printf("a:%d,b:%d,c:%d,d:%d\n",a,b,c,d);
+    c = a && b;
+    printf("a:%d,b:%d,c:%d\n", a, b, c);
+    c=a||b;
+    printf("a:%d,b:%d,c:%d\n",a,b,c);
+    c=++a&&b;
+    printf("a:%d,b:%d,c:%d\n",a,b,c);
+    c=++a||b;
+    printf("a:%d,b:%d,c:%d\n",a,b,c);
+    c=a&&++b;
+    printf("a:%d,b:%d,c:%d\n",a,b,c);
+    c=a||++b;
+    printf("a:%d,b:%d,c:%d\n",a,b,c);
+    c=++a&&++b;
+    printf("a:%d,b:%d,c:%d\n",a,b,c);
+    c=++a||++b;
+    printf("a:%d,b:%d,c:%d\n",a,b,c);
+    d = a && b || c;
+    printf("a:%d,b:%d,c:%d,d:%d\n",a , b, c, d);
 }
