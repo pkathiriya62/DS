0590eddbaa0928b9d385a0827d6ead1bc44e09b9
Prince kathiriya <princekathiriya62@gmail.com>
2023-08-16 18:35:35 +0530

    ds

---
+++ operator.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a=1,b=2,c;
+    c=a&&b;
+    printf("a:%d,b:%d,c:%d\n",a,b,c);
+    // c=a||b;
+    // printf("a:%d,b:%d,c:%d\n",a,b,c);
+    // c=++a&&b;
+    // printf("a:%d,b:%d,c:%d\n",a,b,c);
+    // c=++a||b;
+    // printf("a:%d,b:%d,c:%d\n",a,b,c);
+    // c=a&&++b;
+    // printf("a:%d,b:%d,c:%d\n",a,b,c);
+    // c=a||++b;
+    // printf("a:%d,b:%d,c:%d\n",a,b,c);
+    // c=++a&&++b;
+    // printf("a:%d,b:%d,c:%d\n",a,b,c);
+    // c=++a||++b;
+    // printf("a:%d,b:%d,c:%d\n",a,b,c);
+}
