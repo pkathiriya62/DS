27aee56c37cef8d404ca4fac1d31276c1bb18154
Prince kathiriya <princekathiriya62@gmail.com>
2023-08-21 18:12:53 +0530

    ds

--- fifo.c
+++ fifo.c
@@ -1,49 +1,69 @@
 #include <stdio.h>
+
 #define n 5
-int a[n], top = -1;
-int insertEnd(int val)
-{
-    if (top >= n - 1)
-        printf("Array is full......\n");
-    else
-    {
+int a[n];
+int top = -1, i;
+
+int display() {
+    if (top < 0) {
+        printf("\nArray is empty .....");
+    } else {
+        for (i = 0; i <= top; i++) {
+            printf("  %d", a[i]);
+        }
+    }
+}
+
+int firstInsert(int value) {
+    if (top >= n - 1) {
+        printf("\n Array is full :");
+    } else {
+        for (int i = top; i >= 0; i--) {
+            a[i + 1] = a[i];
+        }
+        a[0] = value;
+        top++;
+    }
+}
+
+int firstDelete() {
+    int x;
+    if (top < 0) {
+        printf("\n Array is already empty :");
+    } else {
+        x = a[0];
+        for (int i = 0; i < top; i++) {
+            a[i] = a[i + 1];
+        }
+        top--;
+        return x;
+    }
+}
+
+int insertEnd(int val) {
+    if (top >= n - 1) {
+        printf("\n Array is full :");
+    } else {
         top++;
         a[top] = val;
     }
 }
-int display()
-{
-    if (top < 0)
-        printf("Array is empty...\n");
-    else
-    {
-        for (int i = 0; i <= top; i++)
-            printf("%d", a[i]);
-    }
-}
-int delete()
-{
-    if (top < 0)
-        printf("array is empty......\n");
-    else
-    {
+
+int delete() {
+    int x;
+    if (top < 0) {
+        printf("Array is already empty :");
+    } else {
+        x = a[top];
         top--;
+        return x;
     }
 }
-int main()
-{
-    insertEnd(10);
-    insertEnd(20);
-    insertEnd(30);
-    insertEnd(40);
-    insertEnd(50);
-    insertEnd(60);
-    // insertEnd(70);
-    delete ();
-    delete ();
-    delete ();
-    display();
-   
 
-
+int main() {
+  firstInsert(100);
+  firstInsert(200);
+  firstInsert(300);
+  delete ();
+  display();
 }
