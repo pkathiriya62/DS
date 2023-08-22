25cc1da9a78c93adcc65747f8f14d9f74acebc21
Prince kathiriya <princekathiriya62@gmail.com>
2023-08-22 17:53:04 +0530

    ds

---
+++ binary_search.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int binarySearch(int array[], int size, int x) {
+    int l = 0;
+    int h = size - 1;
+
+    while (l <= h) {
+        int mid = l + (h - l) / 2;
+
+        if (array[mid] == x) {
+            return mid+1; 
+        } else if (array[mid] < x) {
+            l = mid + 1; 
+        } else {
+            h = mid - 1; 
+        }
+    }
+
+    return -1; 
+}
+
+int main() {
+    int array[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int size = 10; 
+    int x;
+    printf("\t2,  4,  6,  8,  10,  12,  14,  16,  18,  20\n");
+
+    printf("enter your element to search in array if available. :");
+    scanf("%d",&x);
+
+    int position = binarySearch(array, size, x);
+
+    if (position != -1) {
+        printf("element %d found at index %d\n", x, position);
+    } else {
+        printf("element %d not found in the array\n", x);
+    }
+}
