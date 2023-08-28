5fcd81c877ff1ab8c1302e2d4920a750b780369b
Prince kathiriya <princekathiriya62@gmail.com>
2023-08-28 18:53:31 +0530

    ds

---
+++ binary_search_using_recurson.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int l, int h, int x) {
+    if (l <= h) {
+        int mid = (l+h) / 2;
+        if (arr[mid] == x) {
+            return mid; 
+        }
+        if (arr[mid] < x) {
+            return binarySearch(arr, mid + 1, h, x); 
+        }
+        return binarySearch(arr, l, mid - 1, x);
+    }
+    return -1; 
+}
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d sorted elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int x;
+    printf("Enter the searching value: ");
+    scanf("%d", &x);
+
+    int p = binarySearch(arr, 0, n - 1, x);
+
+    if (p != -1) {
+        printf("Element found at index %d\n", p+1);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
