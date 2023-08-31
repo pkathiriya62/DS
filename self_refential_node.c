c4f3d8caa855589d4c2977e2b233dae838032dff
Prince kathiriya <princekathiriya62@gmail.com>
2023-08-31 18:08:48 +0530

    ds

--- self_refential_node.c
+++ self_refential_node.c
@@ -5,7 +5,7 @@ struct node{
     struct node*ptr;
 };
 int main(){
-    struct node a,b;
+    struct node a,b,c,d;
     a.ch='A';
     a.data=50;
     a.ptr=NULL;
@@ -14,9 +14,22 @@ int main(){
     b.data=105;
     b.ptr=NULL;
 
+    c.ch='B';
+    c.data=100;
+    c.ptr=NULL;
+
+    d.ch='b';
+    d.data=107;
+    d.ptr=NULL;
+    
+
     a.ptr=&b;
     b.ptr=&a;
+    c.ptr=&d;
+    d.ptr=&c;
 
     printf("b:data:%d\tchar:%c\n",a.ptr->data,a.ptr->ch);
     printf("a:data:%d\tchar:%c\n",b.ptr->data,b.ptr->ch);
+    printf("c:data:%d\tchar:%c\n",c.ptr->data,c.ptr->ch);
+    printf("d:data:%d\tchar:%c\n",d.ptr->data,d.ptr->ch);
 }
